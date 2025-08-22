58b8f074624bfc3d402ea77e0459efd36a7be511
yazid <yazidouz03@gmail.com>
2025-08-22 16:30:29 +0100

    data quality check gold

---
+++ tests/data_quality_gold.sql
@@ -0,0 +1,52 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency, 
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+
+Usage Notes:
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+-- ====================================================================
+-- Checking 'gold.dim_customers'
+-- ====================================================================
+-- Purpose: Ensure that each customer_key in gold.dim_customers is unique.
+-- Expectation: No rows should be returned (no duplicates).
+SELECT 
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.dim_products'
+-- ====================================================================
+-- Purpose: Ensure that each product_key in gold.dim_products is unique.
+-- Expectation: No rows should be returned (no duplicates).
+SELECT 
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.fact_sales' relationships
+-- ====================================================================
+-- Purpose: Validate referential integrity between fact_sales and dimensions.
+-- Expectation: No rows should be returned (all foreign keys have matching dimensions).
+SELECT * 
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL  
